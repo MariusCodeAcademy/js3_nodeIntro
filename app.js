381a12f71b74c6a9d44382ed164285313769fe27
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-17 13:20:46 +0300

    console log backtiks

---
+++ app.js
@@ -0,0 +1,7 @@
+const color = "green";
+
+console.log(`hello world and the color is ${color}`);
+
+// document.getElementById("id"); // nera tokio objekto
+
+// node app.js // paleidziam js faila su node ir gaunam out put
