184637edf6dc581de8831f9a7bf4044b4b6a2a1b
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-18 15:22:36 +0300

    uzd + function export

--- import/modules.js
+++ import/modules.js
@@ -1,4 +1,4 @@
-const { persons, ages } = require("./people");
+const { persons, ages, add } = require("./people");
 // p.push("Karen");
 
-console.log(" p from modules js", persons[0], ages[0]);
+console.log(" p from modules js", persons[0], ages[0], add(100, 500));
--- import/people.js
+++ import/people.js
@@ -1,9 +1,15 @@
 const persons = ["James", "Bob", "Janes"];
 const ages = [23, 54, 43];
 
+// const add = (a, b) => a + b;
+function add(a, b) {
+  return a + b;
+}
+
 console.log(" persons", persons);
 
 module.exports = {
   persons,
   ages,
+  add,
 };
