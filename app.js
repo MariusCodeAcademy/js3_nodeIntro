f24be52c3885b0fde3591233e67eb59630126f11
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-18 13:52:01 +0300

    move js code to app.js

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ console.log(`hello world and the color is ${color}`);
 // document.getElementById("id"); // nera tokio objekto
 
 // node app.js // paleidziam js faila su node ir gaunam out put
+// isikeliam failu sistemo moduli
+
+const fs = require("fs");
+
+fs.writeFile("user.txt", "username=Bob", (err) => {
+  if (err) {
+    console.log(" err", err);
+  } else {
+    console.log("irasytas failas");
+  }
+});
