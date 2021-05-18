c90641ea2ed25428aab6e426be9885bc564e69f7
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-18 12:02:13 +0300

    fs write to file

--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
 console.log("1 test");
+
+// isikeliam failu sistemo moduli
+const fs = require("fs");
+
+fs.writeFile("user.txt", "username=Bob", (err) => {
+  if (err) {
+    console.log(" err", err);
+  } else {
+    console.log("irasytas failas");
+  }
+});
